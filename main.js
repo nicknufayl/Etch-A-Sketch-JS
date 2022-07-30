f164e0363fdeeb0cb04f051b52dbb114bc582adb
nicknufayl <nikndaniel@gmail.com>
2022-07-30 16:42:08 +0800

    Add changeSize() and validation

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const populateBoard = (size) => {
     let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
-    for (let i = 0; i < 256; i++) {
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
         square.style.backgroundColor = 'blue';
         board.insertAdjacentElement('beforeend', square);
@@ -12,4 +16,10 @@ const populateBoard = (size) => {
 
 populateBoard(16);
 
-
+const changeSize = (input) => {
+    if (input >= 2 && input <= 100) {
+        populateBoard(input);
+    } else {
+        window.alert('Enter valid number!');
+    }
+}
