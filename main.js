c6f012b124c71d836795f4a37f9e3633ebe96c15
nicknufayl <nikndaniel@gmail.com>
2022-07-30 18:06:09 +0800

    Add basic functionality

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+let color = 'black';
+
 const populateBoard = (size) => {
     let board = document.querySelector('.board');
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.remove());
-
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
     let amount = size * size;
     for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
-        square.style.backgroundColor = 'blue';
-        board.insertAdjacentElement('beforeend', square);
+        square.addEventListener('mouseover', colorSquare);
+        square.addEventListener('mousedown', colorSquare);
+        square.style.backgroundColor = 'white';
+        board.appendChild(square);
     }
 }
 
@@ -23,3 +30,23 @@ const changeSize = (input) => {
         window.alert('Enter valid number!');
     }
 }
+
+function colorSquare(e) { 
+    if (e.type === 'mouseover' && !mouseDown) return;
+    else if (color === "random") {
+        e.target.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        e.target.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
